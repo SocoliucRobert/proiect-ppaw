57b2d455cfb62f6f0779d7040b5ac6126de8fede
SocoliucRobert <ionut.socoliuc@student.usv.ro>
2024-11-05 10:33:24 +0200

    update

--- src/testClient.js
+++ src/testClient.js
@@ -1,7 +1,7 @@
 
 import React, { useEffect, useState } from 'react';
 
-import { UserAccessor } from './bazadate/DatabaseFirst'; 
+import { UserAccessor } from './bazadate/databaseFirst'; 
 
 const DemoTable = () => {
 
